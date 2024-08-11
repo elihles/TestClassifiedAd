e02e5eba3e29018e330d2d725629558887c28a0f
elihl <elihl@LAPTOP-EVR5KTER>
2024-08-11 10:56:32 +0200

    Add project files.

---
+++ TestClassifiedAd/Classified.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    internal class Classified
+    {
+        private string Category;
+        private int  NumberWords;
+        private double price;
+
+        public Classified(string category, int numberWords)
+        {
+            Category = category;
+            NumberWords = numberWords;
+            CalculatePrice();
+        }
+        public string getCategory()
+        {
+            return Category;
+        }
+
+        public void setCategory(string category)
+        {
+            this.Category = category;
+        }
+        public int getNumberWords()
+        {
+            return NumberWords;
+           
+        }
+        public void setNumberWords(int numberWords)
+        {
+             this.NumberWords = numberWords;
+            CalculatePrice();
+        }
+        public void CalculatePrice()
+        {
+            price = NumberWords * 0.09;
+        }
+      
+        public void DisplayAd()
+        {
+            Console.WriteLine("Classified Ad:");
+            Console.WriteLine("Category   :{0} ",Category);
+            Console.WriteLine("Number of words :{0}",NumberWords);
+            Console.WriteLine("Price     :{0:C}",price);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ TestClassifiedAd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Classified ad1 = new Classified("Used Cars", 50);
+            Classified ad2 = new Classified("Real Estate", 100);
+            Console.WriteLine("Category of first ad: " + ad1.getCategory());
+            Console.WriteLine();
+
+            // Update the number of words for the second object
+            ad2.setNumberWords(120);
+
+            // Use the display method to display the values of the fields for each object
+            ad1.DisplayAd();
+            ad2.DisplayAd();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
